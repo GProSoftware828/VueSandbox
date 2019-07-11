bfebd5b9aa7e44ef48bde6db8cade02b07c91aaa
GproSoftware828 <JetSendProductions@Gmail.com>
2019-07-11 22:41:11 +1000

    Added contact form with a slot

--- sample-project/src/main.js
+++ sample-project/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import Announcement from './Announcement.vue';
 import Article from './news/components/Article.vue';
+import ContactUs from './news/components/Contact.vue';
 
 export const eventBus = new Vue();
 export const announcementEventBus = new Vue();
 
 Vue.component('announcement', Announcement);
 Vue.component('appArticle', Article); //HTML5 has tag named "Article"
+Vue.component('ContactUs', ContactUs);
 
 new Vue({
   el: '#app',
